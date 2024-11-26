9de9fde377d357b18c665ee0579dc6eb8b06633a
karima belkhatir <karima.belkhatir96@gmail.com>
2024-11-26 20:55:55 +0100

    Modif5

--- app.js
+++ app.js
@@ -27,20 +27,20 @@ menu_item.forEach((item) => {
 //----------------------------------------------------------------------------------------------------------------
 // empêcher que votre visiteur effectue un clic droit sur votre page.
 
-function disableselect(e){ 
-	return false 
-} 
-function reEnable(){ 
-return true 
-} 
-//if IE4+ 
-document.onselectstart=new Function ("return false") 
-document.oncontextmenu=new Function ("return false") 
-//if NS6 
-if (window.sidebar){ 
-document.onmousedown=disableselect 
-document.onclick=reEnable 
-} 
+// function disableselect(e){ 
+// 	return false 
+// } 
+// function reEnable(){ 
+// return true 
+// } 
+// //if IE4+ 
+// document.onselectstart=new Function ("return false") 
+// document.oncontextmenu=new Function ("return false") 
+// //if NS6 
+// if (window.sidebar){ 
+// document.onmousedown=disableselect 
+// document.onclick=reEnable 
+// } 
 
 //----------------------------------------------------------------------------------------------------------------
 // pour permetre de copier le mail et le tel
